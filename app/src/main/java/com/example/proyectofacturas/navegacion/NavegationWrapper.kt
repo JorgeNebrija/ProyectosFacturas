e179699f300fdb44e253146cc8c03e3d1f60cba5
Jorge <jorgeanton1810@gmail.com>
2025-02-10 14:18:28 +0100

    Creación pantallas

---
+++ app/src/main/java/com/example/proyectofacturas/navegacion/NavegationWrapper.kt
@@ -0,0 +1,22 @@
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavigationWrapper() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "pantallaMenu"
+    ) {
+        composable("pantallaMenu") { PantallaMenu(navController) }
+        composable("pantallaCreacionVenta") { PantallaCreacionVenta(navController) }
+        composable("pantallaCreacionCompra") { PantallaCreacionCompra(navController) }
+        composable("pantallaActualizar") { PantallaActualizar(navController) }
+        composable("pantallaBorrar") { PantallaBorrar(navController) }
+        composable("pantallaMostrar") { PantallaMostrar(navController) }
+    }
+}
