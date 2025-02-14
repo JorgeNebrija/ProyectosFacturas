967b6ff348903f834e92fff9232cc2ffb2e9c22d
=Samuel_Navasardyan <=sur00044@gmail.com>
2025-02-14 10:52:02 +0100

    Diseño mejorado de la pantalla de detalles de las facturas

--- app/src/main/java/com/example/proyectofacturas/vistas/PantallaDetalleFactura.kt
+++ app/src/main/java/com/example/proyectofacturas/vistas/PantallaDetalleFactura.kt
@@ -19,6 +19,7 @@ import com.example.proyectofacturas.componentes.Header
 import com.example.proyectofacturas.modelos.Factura
 import com.example.proyectofacturas.ui.theme.AzulPrincipal
 import com.example.proyectofacturas.ui.theme.colorDeFondo
+import com.example.proyectofacturas.ui.theme.colorTitulo
 import com.example.proyectofacturas.viewmodels.FacturaViewModel
 
 @Composable
@@ -27,6 +28,8 @@ fun PantallaDetalleFactura(
     idFactura: String,
     facturaViewModel: FacturaViewModel
 ) {
+
+
     val factura by facturaViewModel.obtenerFacturaPorId(idFactura).observeAsState()
 
     Scaffold(
@@ -58,43 +61,74 @@ fun PantallaDetalleFactura(
 @Composable
 fun DetalleFactura(factura: Factura) {
     Column(modifier = Modifier.padding(16.dp)) {
-        Text(
-            "Factura N.º: ${factura.numeroFactura}",
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            modifier = Modifier.padding(bottom = 8.dp)
-        )
-        Text("Fecha de emisión: ${factura.fecha}")
-
-        Spacer(modifier = Modifier.height(16.dp))
-
-        SeccionDetalle("Datos del Emisor") {
-            Text("Empresa: ${factura.nombre}")
-            Text("NIF: ${factura.cif}")
-            Text("Dirección: ${factura.direccion}")
+        SeccionDetalle("Detalles de la Factura N.º: ${factura.numeroFactura}") {
+            Text("Fecha de emisión: ${factura.fecha}")
         }
 
-        SeccionDetalle("Datos del Receptor") {
-            Text("Cliente: ${factura.cliente}")
-            Text("NIF: ${factura.cifCliente}")
-            Text("Dirección: ${factura.direccion}")
+        Text(
+            "Datos de la Factura",
+            fontWeight = FontWeight.Bold,
+            fontSize = 16.sp,
+            modifier = Modifier.padding(top = 16.dp, bottom = 8.dp),
+            color = Color.Black
+
+        )
+        Row(
+            modifier = Modifier
+
+                .fillMaxWidth()
+        ) {
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(150.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color.White)
+                    .padding(16.dp)
+
+            ) {
+                Text("Datos del Emisor", fontWeight = FontWeight.Bold, color = colorTitulo)
+               Spacer(modifier = Modifier.height(8.dp))
+                Text("Empresa: ${factura.nombre}")
+                Text("NIF: ${factura.cif}")
+                Text("Dirección: ${factura.direccion}")
+            }
+            Spacer(modifier = Modifier.width(16.dp))
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(150.dp)
+                    .clip(RoundedCornerShape(8.dp))
+                    .background(Color.White)
+                    .padding(16.dp)
+            ) {
+                Text("Datos del Receptor", fontWeight = FontWeight.Bold, color = colorTitulo)
+                Spacer(modifier = Modifier.height(8.dp))
+                Text("Cliente: ${factura.cliente}")
+                Text("NIF: ${factura.cifCliente}")
+                Text("Dirección: ${factura.direccionCliente}")
+            }
         }
 
         SeccionDetalle("Importes de la Factura") {
-            Text("Base imponible: ${factura.baseImponible}")
-            Text("IVA: ${factura.iva}")
-            Text("Total: ${factura.total}")
+            Text("Base imponible: ${factura.baseImponible}€")
+            Text("IVA: ${factura.iva}%")
+            Spacer(modifier = Modifier.height(8.dp))
+            Text("Total: ${factura.total}€", fontWeight = FontWeight.Bold)
         }
     }
 }
 
 @Composable
 fun SeccionDetalle(titulo: String, contenido: @Composable ColumnScope.() -> Unit) {
+
     Text(
         text = titulo,
         fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
-        modifier = Modifier.padding(top = 16.dp, bottom = 8.dp)
+        modifier = Modifier.padding(top = 16.dp, bottom = 8.dp),
+        color = Color.Black
+
     )
     Column(
         modifier = Modifier
