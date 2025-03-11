1899a826db5f03575f9d4049ecae035e33089efb
Alejandro Yepes <alejandroyg3@gmail.com>
2025-03-11 23:32:10 +0100

    Solucion problema biometrico, sharedPreference añadido en AppNavigation

--- app/src/main/java/com/example/proyectofacturas/navegacion/AppNavegation.kt
+++ app/src/main/java/com/example/proyectofacturas/navegacion/AppNavegation.kt
@@ -1,6 +1,8 @@
 package com.example.proyectofacturas.navegacion
 
+import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -19,10 +21,22 @@ import com.example.proyectofacturas.vistas.PantallaVideo
 
 @Composable
 fun NavigationWrapper() {
+    val context = LocalContext.current
     val navController = rememberNavController()
     val facturaViewModel: FacturaViewModel = viewModel() // Crear el ViewModel aquí
 
-    NavHost(navController = navController, startDestination = "pantallaAutenticacion") {
+    // Accede a SharedPreferences para verificar si el usuario ha iniciado sesión antes
+    val sharedPref = context.getSharedPreferences("UserSession", Context.MODE_PRIVATE)//Evita que otras apps puedan acceder a la informacion
+    val haIniciadoSesionAntes = sharedPref.getBoolean("hasLoggedInBefore", false)
+
+    // Define la pantalla inicial en función del valor de SharedPreferences
+    val pantallaInicial = if (haIniciadoSesionAntes) {
+        "pantallaAutenticacion" // Autenticación biométrica
+    } else {
+        "pantallaLogin" // Pantalla de login normal
+    }
+
+    NavHost(navController = navController, startDestination = pantallaInicial) {
 
         composable("pantallaLogin") { PantallaLogin(navController) }
 
@@ -30,29 +44,32 @@ fun NavigationWrapper() {
 
         composable("pantallaAutenticacion") { PantallaAutenticacionBiometrica(navController) }
 
-
         composable("facturas") {
             PantallaFacturas(navController, facturaViewModel) // Pasar el ViewModel
         }
+
         composable("pantallaVideo") {
             PantallaVideo(navController)
         }
+
         composable("crear_factura") {
             PantallaCrearFactura(navController, facturaViewModel) // Pasar el ViewModel
         }
+
         composable("detalle_factura/{facturaId}") { backStackEntry ->
             val facturaId = backStackEntry.arguments?.getString("facturaId") ?: ""
             PantallaDetalleFactura(navController, facturaId, facturaViewModel) // Pasar el ID
         }
+
         composable("perfil") {
             PantallaPerfil(navController, facturaViewModel) // Pasar el ViewModel
         }
+
         composable("editar_factura/{id}") { backStackEntry ->
             val id = backStackEntry.arguments?.getString("id") ?: ""
             PantallaEditarFactura(navController, id, facturaViewModel)
         }
-
-
     }
 }
+
 
